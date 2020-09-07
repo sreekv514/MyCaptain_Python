2a0c2c6eb786f16cd6f52b87f57b7a4bffb71eef
Vandana Sreekumar <66350072+sreekv514@users.noreply.github.com>
2020-09-07 13:09:57 +0530

    Compute area of a circle
    Program to read the radius of a circle from the user and then calculate and print the area of the circle.

---
+++ area_circle.py
@@ -0,0 +1,5 @@
+# Program to read radius of a circle from user and calculate area
+from math import pi
+radius = float(input("Enter radius of circle: "))
+area = math.pi*radius**2
+print("Area of circle with radius", radius, " is :", area)
